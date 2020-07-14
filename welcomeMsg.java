e8933aee127f3c2f22f0ae02c3f5140ba5ba53b8
swathireddyy <swathithillana12@gmail.com>
2020-07-14 19:56:43 +0530

    Checking employee Attendance

--- welcomeMsg.java
+++ welcomeMsg.java
@@ -1,5 +1,12 @@
 class welcomeMsg{
-	public static void main(String[] args){
-		System.out.println("Welcome to java Programming");
-	}
+   public static void main(String[] args){
+		int IS_FULL_TIME = 1;
+   	double empCheck = Math.floor(Math.random() * 10)%2;
+   	if (empCheck == IS_FULL_TIME)
+      	System.out.println("employee is present");
+   	else
+      	System.out.println("employee is absent");
+   }
 }
+
+
