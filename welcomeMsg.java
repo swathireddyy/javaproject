21ca51b1a41d11e0c0f9626ea40f9310a794a556
swathireddyy <swathithillana12@gmail.com>
2020-07-14 20:40:11 +0530

    Calculating Employee Wage

--- welcomeMsg.java
+++ welcomeMsg.java
@@ -1,11 +1,14 @@
 class welcomeMsg{
    public static void main(String[] args){
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR=20;
+   	int empHrs=0;
+   	int empWage=0;
    	double empCheck = Math.floor(Math.random() * 10)%2;
    	if (empCheck == IS_FULL_TIME)
-      	System.out.println("employee is present");
-   	else
-      	System.out.println("employee is absent");
+      	empHrs=8;
+   	empWage=empHrs*EMP_RATE_PER_HOUR;
+   	System.out.println("EMP WAGE: " + empWage);
    }
 }
 
