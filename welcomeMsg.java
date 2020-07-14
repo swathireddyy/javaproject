b114eb9eaa8fdf39a982aea874fb1efa2c906a2f
swathireddyy <swathithillana12@gmail.com>
2020-07-14 21:15:00 +0530

    Calculating wages till a condition is reached

--- welcomeMsg.java
+++ welcomeMsg.java
@@ -1,15 +1,18 @@
 class welcomeMsg{
 public static final int IS_PART_TIME = 1;
    public static final int IS_FULL_TIME = 2;
-   public static final int EMP_RATE_PER_HOUR=20;
+   public static final int EMP_RATE_PER_HOUR=100;
    public static final int NUM_OF_WORKING_DAYS = 20;
+   public static final int MAX_HRS_IN_A_MONTH = 10;
 
    public static void main(String[] args){
-      int empHrs=0;
-      int empWage=0;
+      int totalEmpHrs=0;
       int totalEmpWage=0;
-      for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-         int empCheck = (int) Math.floor(Math.random() * 10)%3;
+      int totalWorkingDays=0;
+      while (totalEmpHrs <=MAX_HRS_IN_A_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+         int empHrs=0;
+         totalWorkingDays++;
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
          switch (empCheck) {
             case IS_FULL_TIME:
                empHrs=8;
@@ -20,7 +23,8 @@ public static final int IS_PART_TIME = 1;
             default:
                empHrs=0;
          }
-         empWage=empHrs*EMP_RATE_PER_HOUR;
+         totalEmpHrs += empHrs;
+         int empWage=empHrs*EMP_RATE_PER_HOUR;
          totalEmpWage += empWage;
          System.out.println("EMP WAGE: " + empWage);
       }
@@ -28,9 +32,5 @@ public static final int IS_PART_TIME = 1;
 
    }
 }
-
-
-
-
 
 
