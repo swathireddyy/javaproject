3bb42026f4d3a8a9157924097150594673720cb6
swathireddyy <swathithillana12@gmail.com>
2020-07-14 20:51:23 +0530

    Adding the part time employee wage

--- welcomeMsg.java
+++ welcomeMsg.java
@@ -1,15 +1,16 @@
 class welcomeMsg{
-   public static void main(String[] args){
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR=20;
-   	int empHrs=0;
-   	int empWage=0;
-   	double empCheck = Math.floor(Math.random() * 10)%2;
-   	if (empCheck == IS_FULL_TIME)
-      	empHrs=8;
-   	empWage=empHrs*EMP_RATE_PER_HOUR;
-   	System.out.println("EMP WAGE: " + empWage);
+ public static void main(String[] args){
+   int IS_PART_TIME = 1;
+   int IS_FULL_TIME = 2;
+   int EMP_RATE_PER_HOUR=20;
+   int empHrs=0;
+   int empWage=0;
+   double empCheck = Math.floor(Math.random() * 10)%3;
+   if (empCheck == IS_FULL_TIME)
+      empHrs=8;
+   else if (empCheck == IS_PART_TIME)
+      empHrs=4;
+   empWage=empHrs*EMP_RATE_PER_HOUR;
+   System.out.println("EMP WAGE: " + empWage);
    }
 }
-
-
